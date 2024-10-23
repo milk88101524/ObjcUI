5dc4cbea14e04d5fd30d78ec6f53613fc9ed00d1
Han <milk88101524@gmail.com>
2024-10-23 09:51:58 +0800

    Initial Commit

---
+++ ObjcUI/ObjcUI.h
@@ -0,0 +1,18 @@
+//
+//  ObjcUI.h
+//  ObjcUI
+//
+//  Created by Han on 2024/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ObjcUI.
+FOUNDATION_EXPORT double ObjcUIVersionNumber;
+
+//! Project version string for ObjcUI.
+FOUNDATION_EXPORT const unsigned char ObjcUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjcUI/PublicHeader.h>
+
+
